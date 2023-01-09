d25b71cb21e6981134c40280fc20b26441286fbe
OwLu0905 <owlu0905@gmail.com>
2023-01-09 14:10:51 +0800

    add: harpoon keymap

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -7,6 +7,13 @@ vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
 
 -- vim.keymap.set("n", "<C-c>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+-- vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
+-- vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
+-- vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "go1", function() ui.nav_file(1) end)
+vim.keymap.set("n", "go2", function() ui.nav_file(2) end)
+vim.keymap.set("n", "go3", function() ui.nav_file(3) end)
+vim.keymap.set("n", "go4", function() ui.nav_file(4) end)
+
+
+
